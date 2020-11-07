d992138f00cf216da5a122329584ae30fadbe4df
Cob @ CobbleVision <73602547+CobbleVisionAlpha@users.noreply.github.com>
2020-11-07 17:50:15 +0100

    Update CobbleVision_SDK.dart

--- CobbleVision_SDK.dart
+++ CobbleVision_SDK.dart
@@ -1,5 +1,7 @@
 class CobbleVision_API{
 
+  import 'package:mongo_dart/mongo_dart.dart';
+
   // ################################################
   // # Incomplete list of imports and environment setup
   // ################################################
@@ -96,9 +98,16 @@ class CobbleVision_API{
 
         IOClient client = new http.Client()
         Response response = await http.post(this.BaseURL + endpoint, body: json.encode({"price_category":price_category, "publicBool":publicBool, "name": name, "tags":tags, "file":file}), headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
-        
+
+        if(debugging){
+          print(response.toString());
+        }
+
         return response
        }catch(e){
+         if(debugging){
+           print(e.toString())
+         }
          throw Exception(e.toString());
        }
      })}
@@ -107,7 +116,7 @@ class CobbleVision_API{
   // # This function deletes Media from CobbleVision
   // # @async
   // # @function deleteMediaFile()  
-  // # @param {array} IDArray Array of ID's as Strings
+  // # @param {list} IDArray Array of ID's as Strings
   // # @returns {Response} This return the DeleteMediaResponse. The body is in JSON format.
   Future<Response> deleteMediaFile(list IDArray) async{
     Future(() => {
@@ -133,16 +142,22 @@ class CobbleVision_API{
           }
         }
 
-        invalidMediaIDs = checkIDArrayForObjectID(media)
-        if(invalidMediaIDs.length > 0){
+        if(!checkIDArrayForObjectID(media)){
           throw Exception("You provided an invalid media ID.")
         }
 
         IOClient client = new http.Client()
         Response response = await http.delete(this.BaseURL + endpoint + "?id=" + json_encode(IDArray), headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
-        
+
+        if(debugging){
+          print(response.toString());
+        }
+
         return response
        }catch(e){
+         if(debugging){
+           print(e.toString());
+         }
          throw Exception(e.toString());
        }
      })}
@@ -152,8 +167,8 @@ class CobbleVision_API{
   // # Launch a calculation with CobbleVision's Web API. Returns a response object with body, response and headers properties, deducted from npm request module;
   // # @async
   // # @function launchCalculation() 
-  // # @param {array} algorithms Array of Algorithm Names
-  // # @param {array} media Array of Media ID's  
+  // # @param {list} algorithms Array of Algorithm Names
+  // # @param {list} media Array of Media ID's  
   // # @param {string} type Type of Job - Currently Always "QueuedJob"
   // # @param {string} [notificationURL] Optional - Notify user upon finishing calculation!
   // # @returns {Response} This returns the LaunchCalculationResponse. The body is in JSON format.
@@ -181,27 +196,34 @@ class CobbleVision_API{
           }
         }
 
-        invalidMediaIDs = checkIDArrayForObjectID(media)
-        if(invalidMediaIDs.length > 0){
+        if(!checkIDArrayForObjectID(media)){
           throw Exception("You provided an invalid media ID.")
         }
 
-        invalidAlgIDs = checkIDArrayForObjectID(algorithms)
-        if(invalidAlgIDs.length > 0){
+        if(!checkIDArrayForObjectID(algorithms)){
           throw Exception("You provided an invalid algorithm ID.")
         }
 
         var jsonObject = json.decode({"algorithms": algorithms, "media":media, "type":type})
         
         if(notificationURL != null){
-          jsonObject["notificationURL"] = notificationURL
+          if(URI.parse(notificationURL).isAbsolute()){
+            jsonObject["notificationURL"] = notificationURL
+          }
         }
 
         IOClient client = new http.Client()
         Response response = await http.post(this.BaseURL + endpoint, body: jsonObject , headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
         
+        if(debugging){
+          print(response.toString());
+        }
+
         return response
        }catch(e){
+         if(debugging){
+           print(e.toString());
+         }
          throw Exception(e.toString());
        }
      })}
@@ -211,7 +233,7 @@ class CobbleVision_API{
   // # This function waits until the given calculation ID's are ready to be downloaded!
   // # @async
   // # @function waitForCalculationCompletion() 
-  // # @param {array} calculationIDArray Array of Calculation ID's
+  // # @param {list} calculationIDArray Array of Calculation ID's
   // # @returns {Response} This returns the WaitForCalculationResponse. The body is in JSON format.   
   Future<Response> waitForCalculationCompletion(list calculationIDArray) async{
     Future(() => {
@@ -237,15 +259,15 @@ class CobbleVision_API{
           }
         }
 
-        invalidCalcIDs = checkIDArrayForObjectID(calculationIDArray)
-        if(invalidCalcIDs.length > 0){
+        if(!checkIDArrayForObjectID([calculationIDArray])){
           throw Exception("You provided an invalid calc ID.")
         }
+
         bool calculationFinishedBool = false
         while(!calculationFinishedBool){
         
           IOClient client = new http.Client()
-          Response response = await http.get(this.BaseURL + endpoint + "?id=" + json_encode(calculationIDArray), headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
+          Response response = await http.get(this.BaseURL + endpoint + "?id=" + json_encode(calculationIDArray) + "&returnOnlyStatusBool=true", headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
           var responseJSON = json_decode(response.body) as List
           if(responseJSON is of List){
             for(int v=0, v<responseJSON.length, v++){
@@ -263,9 +285,21 @@ class CobbleVision_API{
               calculationFinishedBool=true
             }
           }
+
+          if(calculationFinishedBool==false){
+            wait(3000)
+          }
         }
+
+        if(debugging){
+          print(response.toString());
+        }
+ 
         return response
        }catch(e){
+         if(debugging){
+           print(e.toString());
+         }
          throw Exception(e.toString());
        }
      })}
@@ -280,7 +314,7 @@ class CobbleVision_API{
   Future<Response> deleteCalculation(list IDArray) async{
     Future(() => {
       try{
-        String endpoint = "media"
+        String endpoint = "calculation"
 
         if(this.BaseURL(this.BaseURL.length-1] != "/"){
           throw Exception("BaseURL must end with slash!")
@@ -301,16 +335,22 @@ class CobbleVision_API{
           }
         }
 
-        invalidMediaIDs = checkIDArrayForObjectID(media)
-        if(invalidMediaIDs.length > 0){
-          throw Exception("You provided an invalid media ID.")
+        if(!checkIDArrayForObjectID([IDArray])){
+          throw Exception("You provided an invalid calc ID.")
         }
 
         IOClient client = new http.Client()
         Response response = await http.delete(this.BaseURL + endpoint + "?id=" + json_encode(IDArray), headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
         
+        if(debugging){
+          print(response.toString());
+        }
+
         return response
        }catch(e){
+         if(debugging){
+           print(e.toString());
+         }
          throw Exception(e.toString());
        }
      })}
@@ -322,6 +362,50 @@ class CobbleVision_API{
   // # @param {array} IDArray ID of calculation to return result Array 
   // # @param {boolean} returnOnlyStatusBool Return full result or only status? See Doc for more detailed description!
   // # @returns {Response} This returns the GetCalculationResult. The body is in json format.
+  Future<Response> getCalculationResult(list IDArray, bool returnOnlyStatusBool) async{
+    Future(() => {
+      try{
+        String endpoint = "calculation"
+
+        if(this.BaseURL(this.BaseURL.length-1] != "/"){
+          throw Exception("BaseURL must end with slash!")
+        }
+
+        list<String> keyArray=["IDArray", "Your API Username", "Your API Token"]
+        list<String> valueArray=[IDArray, apiUserName, apiToken]
+        list<String> typeArray=["Array", "String", "String"]
+  
+        try{
+          checkTypeOfParameter(valueArray, typeArray)
+        }catch(e){
+          int err_message=int.parse(e.toString())
+          if(err_message is Integer){
+            throw Exception("The provided data is not valid:" + keyArray[err_message] + "is not of type" + typeArray[err_message])
+          }else{
+            throw Exception(e.toString())
+          }
+        }
+
+        if(!checkIDArrayForObjectID([IDArray])){
+          throw Exception("You provided an invalid calc ID.")
+        }
+      
+        IOClient client = new http.Client()
+        Response response = await http.get(this.BaseURL + endpoint + "?id=" + json_encode(IDArray) + "&returnOnlyStatusBool=" + returnOnlyStatusBool.toString(), headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
+        
+        if(debugging){
+          print(response.toString());
+        }
+
+        return response
+       }catch(e){
+         if(debugging){
+           print(e.toString());
+         }
+         throw Exception(e.toString());
+       }
+     })}
+  }
 
   // # Request your calculation result by ID with the CobbleVision API. Returns a response object with body, response and headers properties, deducted from npm request module;
   // # @async
@@ -332,6 +416,59 @@ class CobbleVision_API{
   // # @param {integer} height target height of visualization file
   // # @returns {Response} This returns the GetCalculationVisualization Result. The body is in binary format. 
   
+  Future<Response> getCalculationVisualization(String id, bool returnBase64Bool, int width, int height) async{
+    Future(() => {
+      try{
+        String endpoint = "calculation/visualization"
+
+        if(this.BaseURL(this.BaseURL.length-1] != "/"){
+          throw Exception("BaseURL must end with slash!")
+        }
+
+        list<String> keyArray=["id", "returnBase64Bool", "width", "height" "Your API Username", "Your API Token"]
+        list<String> valueArray=[id, returnBase64Bool, width, height, apiUserName, apiToken]
+        list<String> typeArray=["String", "Boolean", "Integer", "Integer", "String", "String"]
+  
+        try{
+          checkTypeOfParameter(valueArray, typeArray)
+        }catch(e){
+          int err_message=int.parse(e.toString())
+          if(err_message is Integer){
+            throw Exception("The provided data is not valid:" + keyArray[err_message] + "is not of type" + typeArray[err_message])
+          }else{
+            throw Exception(e.toString())
+          }
+        }
+
+        if(!checkIDArrayForObjectID([id]){
+          throw Exception("You provided an invalid calc ID.")
+        }
+        
+        if(width==0){
+          throw new Exception("Width cannot be zero!")
+        }
+
+        if(height==0){
+          throw new Exception("Height cannot be zero!")
+        }
+
+        IOClient client = new http.Client()
+        Response response = await http.get(this.BaseURL + endpoint + "?id=" + id + "&returnBase64Bool=" + returnBase64Bool.toString() + "&height=" + height + "&width=" + width, headers:{"Content-Type":"application/json", "Accept": "application/json", "Basic": + base64encode(utf8.encode({this.apiUserName:this.apiToken}))
+        
+        if(debugging){
+          print(response.toString());
+        }
+        
+        return response
+       }catch(e){
+         if(debugging){
+           print(e.toString());
+         }
+         throw Exception(e.toString());
+       }
+     })}
+  }
+
   // ###################################################
   // ## Helper Functions
   // ###################################################
@@ -342,21 +479,55 @@ class CobbleVision_API{
   // # @param {array} targetArray Array of values to be checked
   // # @param {array} typeArray Array of types in strings to be checked against
   // # @returns {boolean} Success of Check
+  bool checkTypeOfParameter(targetArray, typeArray){
+    try{
+      for(var s=0; s<targetArray.length; s++){
+        if(!(targetArray[s].runtimeType.toString() === typeArray[s])){
+          if(typeArray[s]==="Array"){
+            if(!(targetArray[s] is of List)){
+              throw Exception(s.toString());
+            }
+          }else{
+            throw Exception(s.toString());
+          }
+        }
+      }
+      return true;
+    }catch(e){
+      if(debugging){
+        print(e.toString());
+      }
+      throw Exception(e.toString())
+    }
+  }
 
   // # Check Array of Mongo IDs for Invalid Values
   // # @sync
   // # @function checkIDArrayForInvalidValues()
-  // # @param {array} IDArray Array of Mongo IDs
+  // # @param {List} IDArray Array of Mongo IDs
   // # @returns {boolean} Success of Check
+  List checkIDArrayForObjectID(List IDArray){
+    try{
+      for(var s = 0; s < IDArray.length; i++){
+         //No Doc for package
+         new ObjectID(IDArray[s])
+      }
+      return true;
+    }catch(e){
+      if(debugging){
+        print(e.toString());
+      }
 
-  // # Verify url using python regex combination
-  // # @sync
-  // # @function validate_url()
-  // # @param {tURL} URL target URL to verify
-  // # @returns {boolean} Success of Check
- 
-  // # Wait using python sleep function
+      return false;
+    }
+  }
+
+  // # Wait using sleep function
   // # @async
   // # @function wait()
   // # @param {number} timeInMS time to wait in ms
   // # @returns {boolean} Success of Wait
+  bool wait(int ms){
+    sleep(ms/1000)
+    return true
+  }
